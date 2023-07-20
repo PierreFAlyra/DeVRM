b04fec19d6fae2d843568d3ef5efed204206d778
PierreFAlyra <pft@pika>
2023-07-20 04:54:22 +0200

    Debug vercel

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,8 +1,56 @@
+"use client"
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  getDefaultWallets,
+  RainbowKitProvider
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, WagmiConfig } from 'wagmi';
+import {
+  hardhat,
+  sepolia
+} from 'wagmi/chains';
+import { publicProvider } from 'wagmi/providers/public';
+
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { WalletProvider } from '@/contexts/WalletContext'
+
+const { chains, publicClient } = configureChains(
+  [hardhat, sepolia],
+  [publicProvider()]
+)
+
+const { connectors } = getDefaultWallets({
+  appName: 'DeVRM',
+  projectId: '343b2052aaac644063b8aa68292db12e',
+  chains
+})
+
+const wagmiConfig = createConfig({
+  autoConnect: false,
+  connectors,
+  publicClient
+})
+
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
       <body>
-        {children}
+        <WagmiConfig config={wagmiConfig}>
+          <RainbowKitProvider chains={chains}>
+            <CacheProvider>
+              <ChakraProvider>
+                <WalletProvider>        
+                  {children}
+                </WalletProvider>
+              </ChakraProvider>
+            </CacheProvider>
+          </RainbowKitProvider>
+        </WagmiConfig>
       </body>
     </html>
   )
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react';
-import Providers from "./providers";
 import App from '@/pages/_app'
 
 export default function Page() {
   return (
     <StrictMode>
-      <Providers>
-        <App />
-      </Providers>
+      <App />
     </StrictMode>
   )
 }
